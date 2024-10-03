06b131b1c93d1ed372d4da3c087d4abd5ca250c9
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2024-10-03 11:00:27 +0000

    A few demo bits

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 
+function getBase() {
+  if (process.env.GITHUB_REPOSITORY) {
+    return `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`;
+  } else {
+    return "/";
+  }
+}
+
 export default defineConfig({
   vite: {
     build: {
       minify: false,
+      base: getBase(),
     },
   },
 });
